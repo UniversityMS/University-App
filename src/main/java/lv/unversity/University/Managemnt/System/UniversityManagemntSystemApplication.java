e331c5855da51ed4cf0b2692ee8ee1fdbf02f4d1
Linda-Ju <linda.junkina@gmail.com>
2021-12-17 21:19:58 +0200

    Initial commit

---
+++ src/main/java/lv/unversity/University/Managemnt/System/UniversityManagemntSystemApplication.java
@@ -0,0 +1,13 @@
+package lv.unversity.University.Managemnt.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityManagemntSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityManagemntSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/unversity/University/Managemnt/System/UniversityManagemntSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.unversity.University.Managemnt.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagemntSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
